0277e8bd7969a0131c4d9034768208c14c217e63
Terje O <terje.osevelle@sharebox.no>
2024-05-29 22:42:50 +0200

    add: linkedList, append & prepend

--- dataStructures.js
+++ dataStructures.js
@@ -70,4 +70,49 @@ function mergeSortedArrays(array1, array2) {
   return mergedArray;
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 4, 4, 6, 30]))
+// console.log(mergeSortedArrays([0, 3, 4, 31], [4, 4, 4, 6, 30]))
+
+
+// Linked list
+// Linked list
+
+class Node {
+  constructor(value) {
+    this.value = value,
+    this.next = null
+  }
+}
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(1);
+console.log(myLinkedList);
+console.log(myLinkedList.append(2));
+console.log(myLinkedList.append(3));
+console.log(myLinkedList.prepend(0));
